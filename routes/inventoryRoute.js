5245dcd7e44c32ef74bbb5afbfde3b7e4050b90d
BoitumeloMeletse <2memusicbookings@gmail.com>
2025-06-04 01:55:45 +0200

    ddd

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -5,15 +5,12 @@ const invController = require("../controllers/invController")
 const utilities = require("../utilities/index")
 
 // Route to build inventory by classification
-router.get("/type/:classificationId", (invController.buildByClassificationId))
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId))
 
-// Route to deliver inventory item details
-router.get("/detail/:invId", (invController.buildByInventoryId))
+// Route to deliver inventory item details (Task 1)
+router.get("/detail/:invId", utilities.handleErrors(invController.buildByInventoryId))
 
 // Route for intentional error (Task 3)
-//router.get("/error", (invController.triggerError))
-
-
-
+router.get("/error", utilities.handleErrors(invController.triggerError))
 
 module.exports = router
